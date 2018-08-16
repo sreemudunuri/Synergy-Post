c33a0c5a0eac6bf3afd6f8d516ab9b9f7e382838
Sree <harsha6raju@yahoo.com>
2018-08-16 11:31:21 -0400

    google api working

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  fetchNews = () => {
+    const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY
+
+    const url = 'https://newsapi.org/v2/top-headlines?' +
+            'country=us&' +
+            `apiKey=${google_api_key}`;
+
+    fetch(url).then(res => res.json()).then(json => this.renderNews(json))
+  }
+
+  componentDidMount() {
+    this.fetchNews()
+  }
+
+  renderNews = (news) => {
+    
+  }
+
+  render() {
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
