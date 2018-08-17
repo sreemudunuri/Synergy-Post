0e8831e16ada371c543430a6fe812862186bab28
github username <github email address>
2018-08-16 23:50:16 -0400

    components render but 0 functionality

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import NewsContainer from "./NewsContainer";
 
 class App extends Component {
+  state = {
+    articles: []
+  };
 
   fetchNews = () => {
-    const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY
+    const google_api_key = process.env.REACT_APP_GOOGLE_API_KEY;
 
-    const url = 'https://newsapi.org/v2/top-headlines?' +
-            'country=us&' +
-            `apiKey=${google_api_key}`;
+    const url =
+      "https://newsapi.org/v2/top-headlines?" +
+      "country=us&" +
+      `apiKey=${google_api_key}`;
 
-    fetch(url).then(res => res.json()).then(json => this.renderNews(json))
-  }
+    fetch(url)
+      .then(res => res.json())
+      .then(json => this.setState({ articles: json.articles }));
+  };
 
   componentDidMount() {
-    this.fetchNews()
+    this.fetchNews();
   }
 
-  renderNews = (news) => {
-    console.log(news.articles)
-  }
+  // renderNews = news => {
+  //   return news.articles;
+  // };
 
   render() {
-
+    console.log(this.state.articles);
     return (
       <div>
-
+        <NewsContainer articles={this.state.articles} />
       </div>
     );
   }
---
+++ src/NewsContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class NewsContainer extends Component {
+  mapInfo = () => {
+    this.props.articles.map(song => console.log(song));
+  };
+
+  renderCards() {
+    {
+      /* please send me your render code */
+    }
+  }
+
+  render() {
+    // console.log(this.props.articles);
+    return null;
+  }
+}
