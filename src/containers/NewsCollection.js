1aa20e63a0e0697e7e26919390508e2e2225365e
github username <github email address>
2018-08-20 11:06:38 -0400

    merged

--- src/containers/NewsCollection.js
+++ src/containers/NewsCollection.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react'
 import NewsCard from '../components/NewsCard';
-
+import Filter from '../components/Filter'
 export default class NewsCollection extends Component{
   sortNewsWithImages = () => {
     return this.props.news.sort((a,b) => (a.urlToImage===null)-(b.urlToImage===null) || +(a>b)||-(a<b))
@@ -10,10 +10,18 @@ export default class NewsCollection extends Component{
   render(){
     console.log(this.sortNewsWithImages())
     return(
-      <div className="grid-container">
-        <Grid> 
-          <NewsCard news={this.sortNewsWithImages()}/>
-        </Grid>
+      <div>
+        <div>
+          <Filter 
+            selectedCountry={this.props.selectedCountry}
+            setCountry={this.props.setCountry}
+          />
+        </div>
+        <div className="grid-container">
+          <Grid> 
+            <NewsCard news={this.sortNewsWithImages()}/>
+          </Grid>
+        </div>
       </div>
     )
   }
---
+++ src/data/countries.js
@@ -0,0 +1,221 @@
+const countries = [
+  {
+    countryCode: "us",
+    countryName: "United States"
+  },
+
+  {
+    countryCode: "ar",
+    countryName: "Argentina"
+  },
+  {
+    countryCode: "au",
+    countryName: "Australia"
+  },
+  {
+    countryCode: "at",
+    countryName: "Austria"
+  },
+  {
+    countryCode: "be",
+    countryName: "Belgium"
+  },
+  {
+    countryCode: "bg",
+    countryName: "Bulgaria"
+  },
+  {
+    countryCode: "br",
+    countryName: "Brazil"
+  },
+  {
+    countryCode: "ca",
+    countryName: "Canada"
+  },
+  {
+    countryCode: "cn",
+    countryName: "China"
+  },
+  {
+    countryCode: "co",
+    countryName: "Colombia"
+  },
+  {
+    countryCode: "cu",
+    countryName: "Cuba"
+  },
+  {
+    countryCode: "cz",
+    countryName: "Czech Republic"
+  },
+  {
+    countryCode: "eg",
+    countryName: "Egypt"
+  },
+  {
+    countryCode: "fr",
+    countryName: "France"
+  },
+  {
+    countryCode: "de",
+    countryName: "Germany"
+  },
+  {
+    countryCode: "gr",
+    countryName: "Greece"
+  },
+  {
+    countryCode: "hk",
+    countryName: "Hong Kong"
+  },
+  {
+    countryCode: "hu",
+    countryName: "Hungary"
+  },
+  {
+    countryCode: "in",
+    countryName: "India"
+  },
+  {
+    countryCode: "id",
+    countryName: "Indonesia"
+  },
+  {
+    countryCode: "ie",
+    countryName: "Ireland"
+  },
+  {
+    countryCode: "il",
+    countryName: "Israel"
+  },
+  {
+    countryCode: "it",
+    countryName: "Italy"
+  },
+  {
+    countryCode: "jp",
+    countryName: "Japan"
+  },
+  {
+    countryCode: "kr",
+    countryName: "Korea"
+  },
+  {
+    countryCode: "lv",
+    countryName: "Latvia"
+  },
+  {
+    countryCode: "lt",
+    countryName: "Lithuania"
+  },
+  {
+    countryCode: "my",
+    countryName: "Malaysia"
+  },
+  {
+    countryCode: "mx",
+    countryName: "Mexico"
+  },
+  {
+    countryCode: "ma",
+    countryName: "Morocco"
+  },
+  {
+    countryCode: "nl",
+    countryName: "Netherlands"
+  },
+  {
+    countryCode: "nz",
+    countryName: "New Zealand"
+  },
+  {
+    countryCode: "ng",
+    countryName: "Nigeria"
+  },
+  {
+    countryCode: "no",
+    countryName: "Norway"
+  },
+  {
+    countryCode: "ph",
+    countryName: "Philippines"
+  },
+  {
+    countryCode: "pl",
+    countryName: "Poland"
+  },
+  {
+    countryCode: "pt",
+    countryName: "Portugal"
+  },
+  {
+    countryCode: "ro",
+    countryName: "Romania"
+  },
+  {
+    countryCode: "ru",
+    countryName: "Russia"
+  },
+  {
+    countryCode: "sa",
+    countryName: "Saudi Arabia"
+  },
+  {
+    countryCode: "rs",
+    countryName: "Serbia"
+  },
+  {
+    countryCode: "sg",
+    countryName: "Singapore"
+  },
+  {
+    countryCode: "sk",
+    countryName: "Slovakia"
+  },
+  {
+    countryCode: "si",
+    countryName: "Slovenia"
+  },
+  {
+    countryCode: "za",
+    countryName: "South Africa"
+  },
+  {
+    countryCode: "se",
+    countryName: "Sweden"
+  },
+  {
+    countryCode: "ch",
+    countryName: "Switzerland"
+  },
+  {
+    countryCode: "tw",
+    countryName: "Taiwan"
+  },
+  {
+    countryCode: "th",
+    countryName: "Thailand"
+  },
+  {
+    countryCode: "tr",
+    countryName: "Turkey"
+  },
+  {
+    countryCode: "ua",
+    countryName: "Ukraine"
+  },
+  {
+    countryCode: "ae",
+    countryName: "United Arab Emirates"
+  },
+  {
+    countryCode: "gb",
+    countryName: "United Kingdom"
+  },
+  {
+    countryCode: "ve",
+    countryName: "Venezuela"
+  }
+];
+
+export default countries;
